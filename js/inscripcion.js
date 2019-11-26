25116810289bc0acc95200356ea59bea1dbed991
matiasal55 <matiasal55@users.noreply.github.com>
2019-11-25 22:28:33 -0300

    carrousel - ejemplo

--- js/inscripcion.js
+++ js/inscripcion.js
@@ -1,38 +1,23 @@
-//function agregarPersona(){
-//    var campoNombre=document.createElement("Input");
-//    campoNombre.setAttribute("type","text");
-//    campoNombre.setAttribute("placeholder","Nombre");
-//    var campoApellido=document.createElement("Input");
-//    campoApellido.setAttribute("type","text");
-//    campoApellido.setAttribute("placeholder","Apellido");
-//    var campoDNI=document.createElement("Input");
-//    campoDNI.setAttribute("type","text");
-//    campoDNI.setAttribute("placeholder","DNI");
-//    var botonEliminar=document.createElement("Input");
-//    botonEliminar.setAttribute("type","button");
-//    botonEliminar.setAttribute("value","-");
-//    botonEliminar.setAttribute("class","addOrDelete");
-//    botonEliminar.setAttribute("onclick","eliminarCampo");
-//    document.getElementById("inscripcion").appendChild(campoNombre);
-//    document.getElementById("inscripcion").appendChild(campoApellido);
-//    document.getElementById("inscripcion").appendChild(campoDNI);
-//    document.getElementById("inscripcion").appendChild(botonEliminar);
-//}
+var num=1;
 
-function agregarPersona(){
+function agregarPersona(precio){
 	var uno = document.getElementById("inscripcion");
 	var dos = uno.cloneNode(true);
     $(dos).children().val("");
 	document.getElementById("contenedor").appendChild(dos);
+    num++;
+    document.getElementById("precio").innerHTML="$"+num*precio;
+
 }
 
-function eliminarPersona() {
-    var uno=this.parentElement;
-    var dos=$(".formulario").first();
-    if(uno.is(dos))
-        $(uno).remove();
-//        $(uno).children().val("");
-//    else
-
+function eliminarPersona(elemento,precio) {
+    var uno =elemento.parentNode;
+    var dos= document.getElementsByClassName("formulario").firstChild;
+    if(!(Object.is(uno,dos))){
+        uno.remove();
+    }else
+        uno.children().val("");
+    num--;
+    document.getElementById("precio").innerHTML="$"+num*precio;
 
 }
